35ff4b011b1c210847e14b74ca5ef78fbc70ea30
Armand Rius Rueda <armand.rius.rueda@gmail.com>
2020-09-22 18:34:31 +0200

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  defaults format: :json do
+    resources :contents, only: :index
+    resources :movies, only: :index
+    resources :seasons, only: :index
+    resources :purchases, only: :create
+    resources :library, only: :index
+  end
 end
