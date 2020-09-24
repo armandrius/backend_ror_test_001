91010c7f42a998428c1a52bd4cf48dd9647a5fb1
Armand Rius Rueda <armand.rius.rueda@gmail.com>
2020-09-24 18:38:19 +0200

    Improve library sorting using sql

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -2,7 +2,7 @@ class LibraryController < ApplicationController
   def index
     @user = User.find params[:user_id]
     @purchases = Rails.cache.fetch "users/#{params[:user_id]}/library", expires_in: 5.minutes do
-      @user.purchases.where('expires_at > created_at').sort_by &:remaining_time_seconds
+      @user.purchases.where('expires_at > created_at').order(Arel.sql "CAST(strftime('%s', expires_at) AS INT) - CAST(strftime('%s', created_at) AS INT)")
     end
   end
 end
